68e3062e12dba5a356109c70a54c7567c75ddbf0
Your New Name <patnidevansh05@gmail.com>
2025-01-15 18:19:13 +0530

    Refactor

---
+++ UserService/src/main/java/com/LostAndFound/UserService/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.LostAndFound.UserService.SecurityConfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    public JWTFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users/saveUser/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
---
+++ UserService/src/main/java/com/LostAndFound/UserService/service/JwtService.java
@@ -0,0 +1,60 @@
+package com.LostAndFound.UserService.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    public boolean validateToken(String token, String number)
+    {
+        String extractedPhoneNumber=extractNumber(token);
+        return (extractedPhoneNumber.equals(number) && !isTokenExpired(token));
+
+    }
+
+    public String extractNumber(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Claims extractAllClaims(String token)
+    {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    boolean  isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    Date extractExpiration(String token)
+    {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private<T> T extractClaim(String token, Function<Claims,T> claimsResolver)
+    {
+        Claims claims=extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    SecretKey getKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+
+    }
+
+
+}
